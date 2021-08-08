f9ff0f2ec26dba3fa1c1ee4c6a04e4437748f7f2
koalakoker <koalakoker@gmail.com>
2021-08-08 07:11:00 +0200

    Get data from the picker and print week and dayoftheweek

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'node_modules/moment';
+
+const weekDayName: Array<string> = [
+  'Domenica',
+  'Lunedi',
+  'Martedi',
+  'Mercoledi',
+  'Giovedi',
+  'Venrdi',
+  'Sabato'
+];
 
 @Component({
   selector: 'app-root',
@@ -7,6 +18,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'calendar';
-  ngOnInit() {    
+  date: any;
+  
+  ngOnInit() {
+  }
+  
+  onChange() {
+    let m = moment(this.date);
+    console.log(m.format());
+    console.log('Week: ' + m.week());    
+    console.log('Giorno della settimana: ' + weekDayName[m.day()]);    
   }
 }
