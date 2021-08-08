97cb7e514042e7453ab913f484d1037a3db1f166
koalakoker <koalakoker@gmail.com>
2021-08-08 07:50:31 +0200

    Working on style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,24 +19,22 @@ const weekDayName: Array<string> = [
 export class AppComponent implements OnInit {
   title = 'calendar';
   date: any;
+  weeks: Array<Array<string>> = [];
   
   ngOnInit() {
   }
   
   onChange() {
     let m = moment(this.date);
-    //console.log(m.format());
-    //console.log('Week: ' + m.week());    
-    //console.log('Giorno della settimana: ' + weekDayName[m.day()]);
     m.subtract(m.date() - 1,'d'); // Go back to first day of the month
     m.subtract(m.day(),'d'); // Go back to first day of the week
     for (let w = 0; w < 5; w++) {
-      let outStr = '';
+      let week = [];
       for (let i = 0; i < 7; i++) {
-        outStr += m.format('Do MMM') + ' ';
+        week[i] = m.format('D');
         m.add(1,'d');
       }
-      console.log(outStr);
+      this.weeks[w] = week;
     }
   }
 }
