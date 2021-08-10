a26a1c4501d8630a7bfe7bc8b6ee8334c715b436
koalakoker <koalakoker@gmail.com>
2021-08-10 06:01:24 +0200

    Showing info coming from package.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import * as moment from 'node_modules/moment';
+import { name, version, author } from '../../package.json';
+
+function toUppercaseFistChar(str: string) {
+  let convertedStr = str[0].toUpperCase();
+  str.toLowerCase();
+  convertedStr += str.substring(1);
+  return convertedStr;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,6 +15,7 @@ import * as moment from 'node_modules/moment';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  info: string = toUppercaseFistChar(name) + " v." + version + " by " + toUppercaseFistChar(author.name);
   
   ngOnInit() {
     moment.updateLocale('it', {
